cb11421432835e754f898543742c171b99b921b4
Lubos Krnac <lubos.krnac@gmail.com>
2024-10-19 07:21:49 +0200

    Correct removal of emojis from directory names

--- src/main/java/net/lkrnac/edupagephotodownloader/PhotosPage.java
+++ src/main/java/net/lkrnac/edupagephotodownloader/PhotosPage.java
@@ -21,7 +21,10 @@ import java.util.regex.Pattern;
 @SuppressWarnings({"java:S106"})
 public class PhotosPage {
     private static final Pattern datePattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}");
-    private static final String EMOJI_PATTERN = "[\\p{So}\\p{Cn}]+";
+    private static final String EMOJI_PATTERN = "[\\p{InEmoticons}\\p{InMiscellaneousSymbolsAndPictographs}\\p{InTransportAndMapSymbols}" +
+            "\\p{InSupplementalSymbolsAndPictographs}\\p{InMiscellaneousSymbols}" +
+            "\\p{InDingbats}\\p{InGeometricShapes}]" +
+            "|\\p{So}|\\uFE0F|\\u200D|\\uD83C[\\uDFFB-\\uDFFF]";
     public static final String DATA_DATE = "data-date";
 
     private final WebDriver driver;
